5abe37c45f2eb904869335e61b9ca0630ce1aee2
Richard Lack <rilack@microsoft.com>
2025-03-21 13:34:55 -0700

    move styling

--- src/components/Flow.tsx
+++ src/components/Flow.tsx
@@ -2,41 +2,23 @@ import React from 'react';
 import Tools from './Tools';
 import FlowPanel from './FlowPanel';
 import PropertyPanel from './PropertyPanel';
+import './Flow.scss';
 
 const Flow: React.FC = () => {
   return (
-    <div style={{ 
-      display: 'flex', 
-      width: '100%', 
-      height: '100%',
-      overflow: 'hidden'
-    }}>
+    <div className="flow-container">
       {/* Left Panel - Tools (25% width) */}
-      <div style={{ 
-        width: '25%', 
-        height: '100%', 
-        borderRight: '1px solid #ccc',
-        overflow: 'auto'
-      }}>
+      <div className="tools-panel">
         <Tools />
       </div>
       
       {/* Middle Panel - Flow Panel (flexible width) */}
-      <div style={{ 
-        flex: 1, 
-        height: '100%',
-        position: 'relative'
-      }}>
+      <div className="flow-panel-container">
         <FlowPanel />
       </div>
       
       {/* Right Panel - Property Panel */}
-      <div style={{ 
-        width: '300px', 
-        height: '100%', 
-        borderLeft: '1px solid #ccc',
-        overflow: 'auto'
-      }}>
+      <div className="property-panel">
         <PropertyPanel />
       </div>
     </div>
