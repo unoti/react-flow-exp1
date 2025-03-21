fc21186ff2fb82e54e48876d5c79854d9922df21
Richard Lack <rilack@microsoft.com>
2025-03-21 13:32:30 -0700

    cleanup after ts conversion

--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Flow } from './Flow';
-export { default as Tools } from './Tools';
-export { default as FlowPanel } from './FlowPanel';
-export { default as PropertyPanel } from './PropertyPanel';
